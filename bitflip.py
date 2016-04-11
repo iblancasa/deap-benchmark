de539be61ccc018df2d661621c03c39b63a518ed
Israel Blancas <iblancasa@gmail.com>
2016-04-11 22:38:04 +0200

    Adding bitflip

---
+++ bitflip.py
@@ -0,0 +1,47 @@
+
+import random
+import time
+import numpy
+from deap import base
+from deap import creator
+from deap import tools
+import sys
+
+
+toolbox = None;
+
+
+def time_maxones(number, chromosome):
+    global toolbox
+    inicioTiempo = time.clock()
+
+    for i in range(number):
+        toolbox.mutate(chromosome)
+
+    return time.clock() - inicioTiempo
+
+
+def main():
+    global toolbox
+    creator.create("fitness", base.Fitness, weights=(1.0,))
+
+    if len(sys.argv)>1 and sys.argv[1]=="numpy":
+        creator.create("chromosome", numpy.ndarray, fitness=creator.fitness)
+    else:
+        creator.create("chromosome", list, fitness=creator.fitness)
+    toolbox = base.Toolbox()
+
+
+    length = 16
+    iterations = 100000
+    top_length = 32768
+
+    while not length > top_length:
+        chromosome = creator.chromosome(random.uniform(0, 1) for _ in range(length))
+        indpb=1/length
+        toolbox.register("mutate", tools.mutFlipBit, indpb=indpb)
+        print("deap-Onemax, " + str(length) +", "+ str(time_maxones( iterations, chromosome)))
+        length = length*2
+
+if __name__ == "__main__":
+    main()
